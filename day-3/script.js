4eea109235222e6852931e0cd78376cbc89bbda9
jpeg25 <jairo25509@hotmail.com>
2021-11-02 22:11:34 -0500

    Cards grid layout.

--- day-3/script.js
+++ day-3/script.js
@@ -4,42 +4,42 @@ const countryName = document.getElementById("country-name");
 const countryCapital = document.getElementById("country-capital");
 const countryDescription = document.getElementById("country-description");
 
-async function getCountry() {
-  try {
-    const response = await fetch("https://restcountries.com/v3.1/alpha/col");
-    if (response.ok) {
-      const jsonResponse = await response.json();
+// async function getCountry() {
+//   try {
+//     const response = await fetch("https://restcountries.com/v3.1/alpha/col");
+//     if (response.ok) {
+//       const jsonResponse = await response.json();
 
-      return jsonResponse;
-    }
-    throw new Error("The request to get the country info failed!");
-  } catch (error) {
-    console.log(error);
-  }
-}
+//       return jsonResponse;
+//     }
+//     throw new Error("The request to get the country info failed!");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-async function filterData(dataFetcher) {
-  const data = await dataFetcher();
-  console.log(data);
-  console.log(data[0].fifa);
-  const {
-    name: { official: officialName },
-    capital,
-    population,
-  } = data[0];
-  return { officialName, capital, population };
-}
+// async function filterData(dataFetcher) {
+//   const data = await dataFetcher();
+//   console.log(data);
+//   console.log(data[0].fifa);
+//   const {
+//     name: { official: officialName },
+//     capital,
+//     population,
+//   } = data[0];
+//   return { officialName, capital, population };
+// }
 
-async function renderData(dataFilterer, dataFetcher) {
-  const filteredData = await dataFilterer(dataFetcher);
+// async function renderData(dataFilterer, dataFetcher) {
+//   const filteredData = await dataFilterer(dataFetcher);
 
-  countryImg.src = "https://source.unsplash.com/featured/?colombia";
-  countryName.textContent = filteredData.officialName;
-  countryCapital.textContent = filteredData.capital[0];
-  countryDescription.textContent = `The ${filteredData.officialName} is a country located in South America. It has ${filteredData.population}. The capital is ${filteredData.capital[0]}.`;
-  console.log(filteredData.officialName, filteredData.capital[0]);
-}
+//   countryImg.src = "https://source.unsplash.com/featured/?colombia";
+//   countryName.textContent = filteredData.officialName;
+//   countryCapital.textContent = filteredData.capital[0];
+//   countryDescription.textContent = `The ${filteredData.officialName} is a country located in South America. It has ${filteredData.population}. The capital is ${filteredData.capital[0]}.`;
+//   console.log(filteredData.officialName, filteredData.capital[0]);
+// }
 
-selectionBtn.addEventListener("click", () => {
-  renderData(filterData, getCountry);
-});
+// selectionBtn.addEventListener("click", () => {
+//   renderData(filterData, getCountry);
+// });
